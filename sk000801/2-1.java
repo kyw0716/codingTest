965f761d34fd9fa81156f8149d0e25773a3d0391
sugamypapa <sk000801@naver.com>
2022-08-31 18:53:59 +0900

    fix: qw-2 성공!

--- sk000801/2-1.java
+++ sk000801/2-1.java
@@ -1,24 +1,24 @@
 package sk000801;
 import java.util.*;
+import java.lang.*;
 
-class solution {
-
-    public String solution(String[] survey, int[] choices) {
-
+class Solution {
+    public static String solution(String[] survey, int[] choices) {
         String answer = "";
-        Map<Character, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<survey.length; i++) {
             if(choices[i]<=3) {
-                Character a = survey[i].charAt(0);
+                Character c = survey[i].charAt(0);
+                String a = c.toString();
                 if(map.containsKey(a)) {
                     if(choices[i]==1) {
-                        map.put(a, map.get(a)+3);
+                        map.replace(a, map.get(a)+3);
                     }
                     if(choices[i]==2) {
-                        map.put(a, map.get(a)+2);
+                        map.replace(a, map.get(a)+2);
                     }
                     if(choices[i]==3) {
-                        map.put(a,map.get(a)+1);
+                        map.replace(a,map.get(a)+1);
                     }
                 }
                 else {
@@ -32,19 +32,19 @@ class solution {
                         map.put(a,1);
                     }
                 }
-
             }
             if(choices[i]>=5) {
-                Character b = survey[i].charAt(1);
+                Character b2 = survey[i].charAt(1);
+                String b = b2.toString();
                 if(map.containsKey(b)) {
                     if(choices[i]==5) {
-                        map.put(b, map.get(b)+1);
+                        map.replace(b, map.get(b)+1);
                     }
                     if(choices[i]==6) {
-                        map.put(b, map.get(b)+2);
+                        map.replace(b, map.get(b)+2);
                     }
                     if(choices[i]==7) {
-                        map.put(b, map.get(b)+3);
+                        map.replace(b, map.get(b)+3);
                     }
                 }
                 else {
@@ -60,15 +60,30 @@ class solution {
                 }  
             }
         }
-        if(map.get('R')>=map.get('T')) answer += "R";
+        String[] set1 = {"R", "T", "C", "F", "J", "M", "A", "N"};
+        for(int j=0; j<set1.length; j++) {
+            if(!map.containsKey(set1[j])) map.put(set1[j], 0); 
+        }
+
+        if((map.get("R"))>=map.get("T")) answer += "R";
         else {answer += "T";}
-        if(map.get('C')>=map.get('F')) answer += "C";
+        if(map.get("C")>=map.get("F")) answer += "C";
         else {answer += "F";}
-        if(map.get('J')>=map.get('M')) answer += "J";
+        if(map.get("J")>=map.get("M")) answer += "J";
         else {answer += "M";}
-        if(map.get('A')>=map.get('N')) answer += "A";
+        if(map.get("A")>=map.get("N")) answer += "A";
         else {answer += "N";}
+        
+        System.out.println(map.get("R"));
         
         return answer;
     }
-}
+
+    public static void main(String[] args) {
+        String sol1[] = {"AN", "CF", "MJ", "RT", "NA"};
+        int sol2[] = {5, 3, 2, 7, 5};
+
+        solution(sol1 , sol2);
+    }
+}    
+
