c75b0bb6f4d4c7371dadeaa89a3a936cb808a1fa
sugamypapa <sk000801@naver.com>
2022-09-06 14:18:02 +0900

    fix: 코테 2w-1
    깔끔하게 수정햇지만 정확도는 똑같..

--- sk000801/로또의 최고 순위와 최저 순위.java
+++ sk000801/로또의 최고 순위와 최저 순위.java
@@ -4,45 +4,44 @@
 class Solution9 {
     public static int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        Arrays.sort(lottos);
-        Arrays.sort(win_nums);
         List<Integer> list = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         for (int i=0; i<6; i++) {
                 list.add(lottos[i]);
                 list2.add(win_nums[i]);
             }
-        int count2 = Collections.frequency(list, 0); //0개수
+        // int zeroCount = Collections.frequency(list, 0); //0개수
+        int count2 = Collections.frequency(list, 0);
         list.retainAll(list2); //중복된 수만 list에 남음
+        // int correctNumber = list.size(); 
 
+        // if(correctNumber < 2) answer[1] = 6;
+        // else answer[1] = 7 - correctNumber;
+        // if(zeroCount + correctNumber < 2) answer[0] = 6;
+        // else answer[0] = 7 - (zeroCount + correctNumber);
         if(count2==0) {
-            if(list.size()==0) {
+            //중복되는 수
+            if(list.size() <= 1) {
                     answer[0] = 6;
                     answer[1] = 6;
                 }
-            for(int i=1; i<=6; i++) {
-                if(list.size()==i) {
-                    answer[0] = 7-i;
-                    answer[1] = 7-i;
-                }
-            }          
+            else{
+                answer[0] = 7-list.size();
+                answer[1] = 7-list.size();
+            }         
         }
-        else { //어쨌든 0이 하나 이상 있으니까 겹치는 수 0~5
-            int count3 = 6-list.size(); //중복되지 않는 수 
-            for(int j=1; j<5; j++) {
-                if(count3==j) {
-                    answer[0] = j+1-count2;
-                    answer[1] = j+1;
-                }
+        else if(count2>0) { //어쨌든 0이 하나 이상 있으니까 겹치는 수 0~5
+            int count3 = 6-list.size(); //중복되지 않는 수
+            if(count3 >= 1 && count3 <= 4){
+                answer[0] = count3 + 1 - count2;
+                answer[1] = count3 + 1;
             }
-            for(int p=5; p<7; p++) {
-                if(count3==p) {
-                    answer[0] = 7-count2;
-                    answer[1] = 6;
-                }
+            else{
+                 answer[0] = 7-count2;
+                 answer[1] = 6;
             }
-    }
-    return answer;
+        }
+        return answer;
 }
 }
 
