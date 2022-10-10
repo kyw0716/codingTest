3f69f7350ac974570f1b8bb5f6d83a10b792d685
sugamypapa <sk000801@naver.com>
2022-10-10 23:51:49 +0900

    feat: 코테 7w-1 수정

--- sk000801/LEVEL2/영어 끝말잇기.java
+++ sk000801/LEVEL2/영어 끝말잇기.java
@@ -2,22 +2,25 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
+        List<String> list = new ArrayList<>();
         for(int i=0; i<words.length; i++) {
             if(words[i].length() <=1) {
                 answer[0] = i%n+1;
                 answer[1] = i/n+1;
                 break;
             }
-            if(i>0 && Collections.frequency(Arrays.asList(words), words[i])>1) {
+            if(i>0 && list.contains(words[i])) {
+                //Collections.frequency(Arrays.asList(words), words[i])>1
                 answer[0] = i%n+1;
                 answer[1] = i/n+1;
                 break;
             }
-            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
+            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0))               {
                 answer[0] = i%n+1;
                 answer[1] = i/n+1;
                 break;
             }
+            list.add(words[i]);
         }
         return answer;
     }
