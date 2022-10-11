4697c38646326c96a98005404b98700b53e63f06
sugamypapa <sk000801@naver.com>
2022-10-11 11:35:06 +0900

    feat: 코테 7w-2

---
+++ sk000801/LEVEL2/짝지어 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            }
+            else stack.push(c);
+        }
+        if(stack.size() == 0) answer = 1;
+        return answer;
+    }
+}
