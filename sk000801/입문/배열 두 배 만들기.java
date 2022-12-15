fa58a9bc0c16fc5cf05ebb8d11fd9a1a5eaea2bb
Cutiepazzipozzi <83707411+Cutiepazzipozzi@users.noreply.github.com>
2022-12-15 21:54:25 +0900

    feat: level0 문제 풀이

---
+++ sk000801/입문/배열 두 배 만들기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] number2 = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            number2[i] = numbers[i]*2;
+        }
+        return number2;
+    }
+}
