f15ff28ffc92ac312a7431a372fca24765958bac
cutiepazzipozzi <sk000801@naver.com>
2023-01-02 16:05:42 +0900

    feat: 백준 DP 문제 풀이

---
+++ sk000801/백준/DP/일이삼더하기.java
@@ -0,0 +1,25 @@
+package sk000801.백준.DP;
+import java.io.*;
+
+public class 일이삼더하기 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 4; i <= 10; i++) {
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
+        }
+
+        for(int i = 0; i < t; i++) {
+            int a = Integer.parseInt(br.readLine());
+
+            System.out.println(dp[a]);
+        }
+    }
+}
