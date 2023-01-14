cb8aaea3e7c647fb040ea0a29815273c343787d0
cutiepazzipozzi <sk000801@naver.com>
2023-01-14 22:17:11 +0900

    feat: 백준 구현 문제 풀이

---
+++ sk000801/백준/구현/AB2.java
@@ -0,0 +1,17 @@
+package sk000801.백준.구현;
+
+import java.util.*;
+
+public class AB2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+}
+
+// A + B - 2 문제
