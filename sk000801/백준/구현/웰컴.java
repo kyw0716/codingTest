a0b22b8a4bf7eb0801698ce7aa2fce676754452e
cutiepazzipozzi <sk000801@naver.com>
2023-01-17 01:03:48 +0900

    feat: 백준 구현 문제 풀이

---
+++ sk000801/백준/구현/웰컴.java
@@ -0,0 +1,7 @@
+package sk000801.백준.구현;
+
+public class 웰컴 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
